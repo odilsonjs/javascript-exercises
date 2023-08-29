f62d64fa9bcd58c4b8d29f2bd08af283331e97ca
Italis Odilson Woodly <italisodilsonwoodly@hotmail.com>
2023-08-29 13:16:31 -0400

    Test_03 successfully completed!

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let stringInArray = string.split("");
+    let stringInArrayReversed = stringInArray.reverse();
+    let joinedStringInArrayReversed = stringInArrayReversed.join("");
+    return joinedStringInArrayReversed;
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
