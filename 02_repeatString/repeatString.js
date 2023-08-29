c25a154dec8c7c8106e77c4d6989c7a9d8d2f175
Italis Odilson Woodly <italisodilsonwoodly@hotmail.com>
2023-08-29 12:41:55 -0400

    Test_02 successfully completed!

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
 
+    let final_string = "";
+    if (num >= 0){
+        for (let i = 0; i < num; i++){
+            final_string += string;
+        }
+        return final_string;
+    } else {
+        return "ERROR";
+    }
+    
 };
 
 // Do not edit below this line
