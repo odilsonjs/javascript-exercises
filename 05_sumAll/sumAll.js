f34f97c9d7fc36d786dcde4bc3bf07ea97908553
Italis Odilson Woodly <italisodilsonwoodly@hotmail.com>
2023-08-29 22:31:06 -0400

    Test_05 succesffully completed!

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,38 @@
-const sumAll = function() {
+const sumAll = function(firstInt, lastInt) {
+   
+    if (Number.isInteger(firstInt) && Number.isInteger(lastInt)){
+        if (firstInt >= 0 && lastInt >= 0){
+        let myArray = [];
+        let sum = 0;
 
-};
+        if(firstInt > lastInt){
+            while(firstInt >= lastInt){
+                myArray.push(firstInt)
+                firstInt--
+            }
+        } else {
+            while(firstInt <= lastInt){
+                myArray.push(firstInt);
+                firstInt++;
+            } 
+
+        }
+    
+        for (let i of myArray){
+            sum += i;
+        }
+        return sum;
+
+        } else {
+            return "ERROR";
+        }
+    }
+    else {
+        return "ERROR";
+    }
+    }
+;
+
 
 // Do not edit below this line
 module.exports = sumAll;
