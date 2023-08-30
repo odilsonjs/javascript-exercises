3e1c3d71a8c658bf43ccd31ffb622b5dcdaa474b
Italis Odilson Woodly <italisodilsonwoodly@hotmail.com>
2023-08-30 09:53:32 -0400

    Test_09_succesfully completed!

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,10 @@
-const palindromes = function () {
+const palindromes = function (theString) {
+     let loweCaseString = theString.replace(/[ .,*+?^$!{}()|[\]\\]/g, "").toLowerCase();
+     let stringReversed = loweCaseString.split("").reverse().join("");
+     return loweCaseString == stringReversed;
+
+    
 
 };
-
 // Do not edit below this line
 module.exports = palindromes;
