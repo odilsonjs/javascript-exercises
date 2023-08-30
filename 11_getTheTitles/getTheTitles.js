ade8a20c25aca1ae71d1bf8fb2e10a8dacdc7443
Italis Odilson Woodly <italisodilsonwoodly@hotmail.com>
2023-08-30 12:19:12 -0400

    Test_11 succesflly completed!

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+    let titles = [];
+    for (let book of array){
+        titles.push(book.title);
+    }
+    return titles;
 };
 
-// Do not edit below this line
 module.exports = getTheTitles;
