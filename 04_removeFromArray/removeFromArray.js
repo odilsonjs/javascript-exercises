296a0f242b504c1c7f58828b3ad58af3f5d4e219
Italis Odilson Woodly <italisodilsonwoodly@hotmail.com>
2023-08-29 20:34:51 -0400

    Test_)$ successfully completed!

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(theArray, ...theElements) {
 
+    for (elem of theElements){
+
+        if (theArray.indexOf(elem) in theArray){    
+            theArray.splice(theArray.indexOf(elem), 1);    
+        }
+        
+    }
+    return theArray;
+    
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
