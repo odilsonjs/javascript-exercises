68bc6f60ad4603bebc8ae5c801b9cb823f6859b5
Italis Odilson Woodly <italisodilsonwoodly@hotmail.com>
2023-08-30 08:56:59 -0400

    Test_08 succesfully completed!

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,48 @@
-const add = function() {
+const add = function(number1, number2) {
+  return number1 + number2;
 	
 };
 
-const subtract = function() {
+const subtract = function(number1, number2) {
+  return number1 - number2
 	
 };
 
-const sum = function() {
+const sum = function(theArray) {
+  let total = 0;
+  for (let i of theArray){
+    total += i
+  }
+  return total;
 	
 };
 
-const multiply = function() {
-
+const multiply = function(theArray) {
+  let product = 1;
+  for (let i of theArray){
+    product *= i
+  }
+  return product;
 };
 
-const power = function() {
+const power = function(number, power) {
+  return number ** power;
 	
 };
 
-const factorial = function() {
+const factorial = function(number) {
+  let theArray = [1];
+  let result = 1;
+  for (let i = 1; i <= number; i++){
+    theArray.push(i);
+  }
+  for(let p of theArray){
+    result *= p;
+  }
+  return result;
 	
 };
+console.log(multiply([2,4]));
 
 // Do not edit below this line
 module.exports = {
