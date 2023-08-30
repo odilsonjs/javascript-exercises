e8d28aff0e638058d2ce695ae7dde899afce3cb3
Italis Odilson Woodly <italisodilsonwoodly@hotmail.com>
2023-08-30 08:20:30 -0400

    Test_07 successfully completed!

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,18 @@
-const convertToCelsius = function() {
+function round(value, precision) {
+  var multiplier = Math.pow(10, precision || 0);
+  return Math.round(value * multiplier) / multiplier;
+}
+
+
+const convertToCelsius = function(degreeFahrenheit) {
+    return round((degreeFahrenheit - 32) / 1.8, 1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degreeCelcius) {
+  return round((degreeCelcius * 1.8) + 32, 1);
 };
+
+console.log(convertToFahrenheit(32));
 
 // Do not edit below this line
 module.exports = {
