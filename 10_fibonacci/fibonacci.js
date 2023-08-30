a7e6d8e9ff9820b7188f2657010d8ed8dab3aea2
Italis Odilson Woodly <italisodilsonwoodly@hotmail.com>
2023-08-30 11:56:17 -0400

    test_10 sucessfully completed!

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,23 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    let counter = 1;
+    let fibonacciSequence = [1, 1];
+    number = Number(number);
+    if (number == 1){
+        return fibonacciSequence[fibonacciSequence.length - 1];    
+    } else if (number == 2) {
+        return fibonacciSequence[fibonacciSequence.length - 1];
+    } else if (number < 0){
+        return "OOPS"
+    } else {
+        while(counter <= number-2){
+            fibonacciSequence.push(fibonacciSequence[fibonacciSequence.length - 1] + fibonacciSequence[fibonacciSequence.length - 2]);
+            counter++;
+        }
+        return fibonacciSequence[fibonacciSequence.length - 1];
+    }
+    
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;
